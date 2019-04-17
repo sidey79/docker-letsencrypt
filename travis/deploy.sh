c68d5432c6ee847b803b9099f03a10b85ecac675
sidey79 <7968127+sidey79@users.noreply.github.com>
2019-04-17 23:21:27 +0200

    Update deploy.sh

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -9,11 +9,11 @@ DOCKER_USER="Sidey79"
 DOCKER_IMAGE="rpi-nginx-letsencrypt"
 DOCKER_TAG="latest"
 
-buildctl build --frontend dockerfile.v0 \
-        --frontend-opt platform=linux/${PLATFORM} \
-        --frontend-opt filename=./${DOCKERFILE_LOCATION} \
-        --exporter image \
-        --exporter-opt name=docker.io/${DOCKER_USER}/${IMAGE}:${TAG}-${PLATFORM} \
-        --exporter-opt push=true \
-        --local dockerfile=. \
-        --local context=.
+#buildctl build --frontend dockerfile.v0 \
+#        --frontend-opt platform=linux/${PLATFORM} \
+#        --frontend-opt filename=./${DOCKERFILE_LOCATION} \
+#        --exporter image \
+#        --exporter-opt name=docker.io/${DOCKER_USER}/${IMAGE}:${TAG}-${PLATFORM} \
+#        --exporter-opt push=true \
+#        --local dockerfile=. \
+#        --local context=.
