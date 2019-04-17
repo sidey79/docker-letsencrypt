7979f6b5c4954c722781cf20fe09a004c0874695
sidey79 <7968127+sidey79@users.noreply.github.com>
2019-04-17 22:51:22 +0200

    Create build.sh

---
+++ travis/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Login into docker
+docker login --username $DOCKER_USER --password $DOCKER_PASSWORD
+
+PLATFORM=arm # equivalent to armhf
+DOCKERFILE_LOCATION="./Dockerfile.armhf"
+DOCKER_USER="Sidey79"
+DOCKER_IMAGE="rpi-nginx-letsencrypt"
+DOCKER_TAG="latest"
+
+buildctl build --frontend dockerfile.v0 \
+        --frontend-opt platform=linux/${PLATFORM} \
+        --frontend-opt filename=./${DOCKERFILE_LOCATION} \
+        --exporter image \
+        --exporter-opt name=docker.io/${DOCKER_USER}/${IMAGE}:${TAG}-${PLATFORM} \
+        --exporter-opt push=true \
+        --local dockerfile=. \
+        --local context=.
+
+
+wait
+
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+docker manifest create ${DOCKER_USER}/${IMAGE}:${TAG}
+docker manifest push ${DOCKER_USER}/${IMAGE}:${TAG}
