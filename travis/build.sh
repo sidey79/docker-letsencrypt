b8fa97781492bc28c1943c0aea3675f6262bd2df
sidey79 <7968127+sidey79@users.noreply.github.com>
2019-04-18 00:18:31 +0200

    Update build.sh

--- travis/build.sh
+++ travis/build.sh
@@ -9,11 +9,11 @@ DOCKER_IMAGE="rpi-nginx-letsencrypt"
 DOCKER_TAG="latest"
 
 buildctl build --frontend dockerfile.v0 \
-       --frontend-opt platform=linux/${PLATFORM} \
-       --frontend-opt filename=./${DOCKERFILE_LOCATION} \
-       --exporter image \
+       --opt platform=linux/${PLATFORM} \
+       --opt filename=./${DOCKERFILE_LOCATION} \
+       --output type=image \
        --exporter-opt name=docker.io/${DOCKER_USER}/${IMAGE}:${TAG}-${PLATFORM} \
        --exporter-opt push=true \
        --local dockerfile=. \
        --local context=. \
-       --frontend-opt build-arg:BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+       --opt build-arg:BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
